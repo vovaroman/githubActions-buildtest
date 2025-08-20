6df5ac8c2eccc1e253d63643d17b64b73ec4f1cc
Vova Roman <vovan.roman7@gmail.com>
2025-08-20 09:13:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-# python3 -m pip install Flask
-from flask import Flask    # dependency
+# app.py
+from flask import Flask
 
-# create an app instance
 app = Flask(__name__)
 
-# define the root path of our API
-@app.route("/")     # decorator (default method is a GET)
-def hello():        # function to run when decorator is interacted with
+@app.route("/")
+def hello():
     return "Hello, DevOps!"
+
+if __name__ == "__main__":
+    app.run()
