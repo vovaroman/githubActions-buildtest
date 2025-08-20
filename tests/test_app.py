c21f9b7540406528ecffd2b332a54e004db47cb9
Vova Roman <vovan.roman7@gmail.com>
2025-08-20 09:04:55 -0400

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,9 @@
 #from app import app
+from ../app import app
 
 def test_home():
     pass
-#    with app.test_client() as client:
-#        response = client.get("/")
-#        assert response.status_code == 200
-#        assert response.data == b"Hello, DevOps!"
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.data == b"Hello, DevOps!"
