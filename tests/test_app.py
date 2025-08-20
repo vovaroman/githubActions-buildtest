4d75147901118329ccc061802a217cbd114b3b2a
Vova Roman <vovan.roman7@gmail.com>
2025-08-20 08:51:59 -0400

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+from app import app
+
+def test_home():
+    pass
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
+        assert response.data == b"Hello, DevOps!"
